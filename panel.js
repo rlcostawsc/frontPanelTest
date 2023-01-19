5cc9e969631e58899b19b657d140ed92af9b9f78
Rodrigo Wanzeler Dos Santos Costa <43542851+rlcostawsc@users.noreply.github.com>
2023-01-19 17:29:29 -0300

    fronpaneltest

--- panel.js
+++ panel.js
@@ -1,10 +1,12 @@
 function go (argument) {
 	
-	var p = document.getElementById("left");
-	var b = document.getElementById("btn");
-	var s = p.className==="min" ? "max":"min";
-	b.innerText==="extender"?b.innerText="recolher":b.innerText="extender";
+	var ctn = document.getElementById("left");
+	var btn_top = document.getElementById("btn_top");
+
+	var s = ctn.className==="min" ? "max":"min";
+	var b = btn_top.className==="btn_top_rec" ? "btn_top_ext" : "btn_top_rec";
 	
-	console.log(b.innerText);
-	p.className = s;
+	console.log(s +" "+b);
+	ctn.className = s;
+	btn_top.className= b;
 }
