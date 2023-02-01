eea9f82d33a2c3fe05954e65c81ba1a56e13aced
Rodrigo Wanzeler Dos Santos Costa <43542851+rlcostawsc@users.noreply.github.com>
2023-02-01 19:20:13 -0300

    developer the bar menu

--- dash.js
+++ dash.js
@@ -3,23 +3,23 @@ let ctx = canvas.getContext('2d');
 ctx.lineWidth=10;
 ctx.shadowColor = "red";
 ctx.shadowBlur = 35;
-let a = 0.036;
+let a = 0.03913;
 let b =1.5;
 let c =0;
 let d = 0
-ctx.arc(100, 75, 50, 1.5 * Math.PI, 4.73);
-ctx.stroke();
+// ctx.arc(100, 75, 50, 1.5 * Math.PI, 4.73);
+// ctx.stroke();
 
 
 function mais () {
 	ctx.beginPath();
 	ctx.clearRect(0, 0, 300, 300);
-	a = a + 0.01;
+	a = a + 0.03913;
 	c = b + a;
 	d = c*Math.PI;
 
 	ctx.arc(100, 75, 50, 1.5 * Math.PI, d);
-	if(c>3.5) a =0.036;
+	if(c>3.5) a =0.039;
 	console.log(d)	
 	ctx.stroke();
 	
@@ -28,7 +28,7 @@ function mais () {
 function menos () {
 	ctx.beginPath();
 	ctx.clearRect(0, 0, 300, 300);
-	a = a - 0.01;
+	a = a - 0.03913;
 	c = b + a;
 	d = c * Math.PI;
 	if(a<0.01){
